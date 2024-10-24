18e3e1e43e380582932508051188d3e311c3ad14
Danish Alam <dredwing05@gmail.com>
2024-10-24 19:33:48 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const socketio = require("socket.io");
+const http = require("http");
+const path = require("path");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+
+io.on("connection", function (socket) {
+  console.log("connected");
+  socket.on("send-location", function (data) {
+    io.emit("recive-location", {
+      id: socket.id,
+      ...data,
+    });
+  });
+  socket.on("disconnect", function () {
+    io.emit("user-disconnected", socket.id);
+  });
+});
+
+app.get("/", function (req, res) {
+  res.render("index");
+});
+
+server.listen(8000, () => {
+  console.log("Server Start");
+});
